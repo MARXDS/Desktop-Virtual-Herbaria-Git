754f5c1de4d486a2731b9e7aff2af7d74050c314
MARXDS <xds19990402@gmail.com>
2022-10-13 16:24:38 +0100

    add other models and change the sky colour

--- Assets/_VirtualHerbaria/Scripts/TimeOfDay.cs
+++ Assets/_VirtualHerbaria/Scripts/TimeOfDay.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class TimeOfDay : MonoBehaviour
 {
-    [SerializeField] private Material _day;
-    [SerializeField] private Material _sunset;
-    [SerializeField] private Material _night;
+   public Material _day;
+   public Material _sunset;
+   public Material _night;
 
-    [SerializeField] private GameObject _sunlight;
-    [SerializeField] private GameObject _sunsetlight;
-    [SerializeField] private GameObject _moonlight;
+   public GameObject _sunlight;
+   public GameObject _sunsetlight;
+   public GameObject _moonlight;
 
 
     public void Day()
